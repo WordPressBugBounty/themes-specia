3eb5318fac2cd95be7aeec6ec5d777a3515e1fa9
Sean Murphy <sean@iamseanmurphy.com>
2025-09-08 11:17:41 +0000

    17.4

--- specia/functions.php
+++ specia/functions.php
@@ -5,7 +5,7 @@ function specia_setup() {
 	/*
 	 * Define Theme Version
 	 */
-	define( 'SPECIA_THEME_VERSION', '17.3' );
+	define( 'SPECIA_THEME_VERSION', '17.4' );
 	
 	/*
 	 * Make theme available for translation.
